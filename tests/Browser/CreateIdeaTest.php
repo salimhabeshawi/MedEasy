2fa3cd5a2966c0c423f88ca59b9d64e80984b469
salimhabeshawi <salimhabeshawi@gmail.com>
2026-06-29 14:06:42 +0300

    Add tests for idea creation flow and implement step completion toggle in StepController

---
+++ tests/Browser/CreateIdeaTest.php
@@ -0,0 +1,24 @@
+<?php
+
+it('creates a new idea', function () {
+    $this->actingAs($user = \App\Models\User::factory()->create());
+
+    visit('/ideas')
+        ->click('@create-idea-button')
+        ->fill('title', 'Some Example Title')
+        ->click('@button-status-completed')
+        ->fill('description', 'An example description')
+        ->fill('@new-link', 'https://salimhabeshawi.com')
+        ->click('@submit-new-link-button')
+        ->fill('@new-link', 'https://google.com')
+        ->click('@submit-new-link-button')
+        ->click('Create')
+        ->assertPathIs('/ideas');
+
+    expect($user->ideas()->first())->toMatchArray([
+        'title' => 'Some Example Title',
+        'status' => 'completed',
+        'description' => 'An example description',
+        'links' => ['https://salimhabeshawi.com', 'https://google.com'],
+    ]);
+});
